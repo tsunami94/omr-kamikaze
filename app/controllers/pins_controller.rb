35b5d6ff3d9fd966ea35b462d4be9550b37b1b87
tuan38 <tuanpt.3894@gmail.com>
2017-01-10 17:12:14 +0700

    Add pins/userasseciation

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
@@ -47,7 +48,8 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
     def correct_user
-      @pin = current_user.pins.find(params[:id])
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
