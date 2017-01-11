94f82a3101ccb5078387b30380b41a6de953df22
tuan38 <tuanpt.3894@gmail.com>
2017-01-11 09:57:58 +0700

    commit message

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -45,7 +45,7 @@ class PinsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
-      @pin = Pin.find(params[:id])
+      @pin = Pin.find_by(id: params[:id])
     end
     def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
@@ -54,7 +54,7 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
   end
 
